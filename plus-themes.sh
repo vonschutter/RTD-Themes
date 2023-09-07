a369ad8538c2962f78fd5b5a1f2ff2e7e1f022b7
Stephan Schutter <stephan.schutter@gmail.com>
2023-09-07 14:50:45 +0200

    ...

--- plus-themes.sh
+++ plus-themes.sh
@@ -27,27 +27,29 @@ VERSION="1.00"
 #::::::::::::::          Script Settings                 ::::::::::::::::::::::
 #::::::::::::::                                          ::::::::::::::::::::::
 #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
 # Ensure administrative privileges.
-[ "$UID" -eq 0 ] || echo -e $YELLOW "This script needs administrative access..." $NO_COLOR
+[ "$UID" -eq 0 ] || echo -e "This script needs administrative access..." 
 [ "$UID" -eq 0 ] || exec sudo -E bash "$0" "$@"
 
 # Put a convenient link to the logs where logs are normally found...
 # capture the 3 first letters as org TLA (Three Letter Acronym)
-: "${_my_scriptdir="$( cd "$( dirname ${BASH_SOURCE[0]} )" && pwd )"}"
+: "${_my_scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"}"
 : "${_tmp="$( mktemp -d )"}"
 : "${_GIT_PROFILE:-"vonschutter"}"
 
 # Determine a reasonable location to place logs:
-_LOG_DIR=/var/log/${_TLA:-"rtd"} ; mkdir -p ${_LOG_DIR}
+_LOG_DIR="/var/log/${_TLA:-"rtd"}" ; mkdir -p "${_LOG_DIR}"
 
 # Determine where to place wallpapers
-_WALLPAPER_DIR=${_WALLPAPER_DIR:-"$(find /opt -name wallpaper)"}
-
+_WALLPAPER_DIR="${_WALLPAPER_DIR:-"$(find /opt -name wallpaper)"}"
+S
 # Location of base administrative scripts and command-lets to get.
-_git_src_url=https://github.com/${_GIT_PROFILE}/${_TLA^^}-Setup.git
+_git_src_url="https://github.com/${_GIT_PROFILE}/${_TLA^^}-Setup.git"
 
 # Determine log file names for this session
-_LOGFILE=${_LOG_DIR}/$(date +%Y-%m-%d-%H-%M-%S-%s)-$(basename $0)-setup.log
+_LOGFILE="${_LOG_DIR}/$(date +%Y-%m-%d-%H-%M)-$(basename $0)-setup.log"
 
 # Likely dependencies that may be needed for installing various themes:
 _potential_dependencies="p7zip-full p7zip p7zip-plugins sassc gettext make git"
@@ -103,23 +105,23 @@ theme::add_global ()
 	--bash | --font | --gtk | --icon | --kde )
 		pushd "${_my_scriptdir}/${1/--/}" || return 1
 		for i in *.7z ; do
-			7z x $i -aoa -o${_tmp}
+			7z x "$i" -aoa -o"${_tmp}"
 			pushd "${_tmp}/${i::-3}"  || return 1
-			bash ./install.sh || ( echo "An error occured while trying to run the install.sh" ; return 1 )
-			popd
+			bash ./install.sh || ( echo "An error occurred while trying to run the install.sh" ; return 1 )
+			popd || return 1
 		done
-		popd
+		popd || return 1
 	;;
 	--wallpaper )
 		chmod 555 -R "${_my_scriptdir}/${1/--/}"
-		if  ps -e |grep "gnome-shell" &>/dev/null ; then 
+		if  pgrep -f "gnome-shell" &>/dev/null ; then 
 			oem::register_wallpapers_for_gnome "${_my_scriptdir}/${1/--/}" || return 1
-		elif  ps -e |grep "plasmashell" &>/dev/null ; then
+		elif  pgrep -f "plasmashell" &>/dev/null ; then
 			echo "Registering wallpapers in: ${_XDG_WALLPAPER_DIR}/"
-			ln -fs "${_my_scriptdir}/${1/--/}"/* ${_XDG_WALLPAPER_DIR}/ || return 1
+			ln -fs "${_my_scriptdir}/${1/--/}"/* "${_XDG_WALLPAPER_DIR}"/ || return 1
 		else 
 			echo "NOT Sure what DE, registering wallpapers in: ${_XDG_WALLPAPER_DIR}/ and /usr/share/backgrounds/"
-			ln -fs "${_my_scriptdir}/${1/--/}"/* ${_XDG_WALLPAPER_DIR}/ || return 1
+			ln -fs "${_my_scriptdir}/${1/--/}"/* "${_XDG_WALLPAPER_DIR}"/ || return 1
 			ln -fs "${_my_scriptdir}/${1/--/}"/* /usr/share/backgrounds/ || return 1
 		fi
 	;;
@@ -152,7 +154,7 @@ dependency::file ()
 	if dependency::search_local "${1}" ; then
 		return 0
 	else
-		if wget ${_src_url} &>/dev/null ; then
+		if wget "${_src_url}" &>/dev/null ; then
 			source ./"${1}"
 			echo "${FUNCNAME[0]} Using: ${_src_url}"
 		else 
@@ -166,44 +168,50 @@ dependency::file ()
 
 dependency::theme_payload ()
 {
-	if echo "$OSTYPE" |grep "linux" ; then
-		echo "Linux OS Found: Attempting to get instructions for Linux..."
-		echo executing $0 >> ${_LOGFILE}
-		if ! hash git &>> ${_LOGFILE} ; then
-			for i in apt yum dnf zypper ; do $i -y install git | tee ${_LOGFILE} ; done
-		fi
-		git clone --depth=1 ${_git_src_url} /opt/${_TLA,,}.tmp | tee ${_LOGFILE}
-			if [ $? -eq 0 ]
+	case "$1" in 
+
+	--download | --desktop )
+		# shellcheck disable=SC2317
+		if echo "$OSTYPE" |grep "linux" ; then
+			system::log_item "Linux OS Found: Attempting to get instructions for Linux..."
+			system::log_item "executing $0"
+			if ! hash git &>> "${_LOGFILE}" ; then
+				system::log_item "git was not found, attmpting to install it..."
+				for i in apt yum dnf zypper ; do $i -y install git | tee "${_LOGFILE}" ; done
+			fi
+			
+			if ! git clone --depth=1 "${_git_src_url}" /opt/"${_TLA,,}".tmp | tee "${_LOGFILE}" ;
 			then
 				echo "Instructions successfully retrieved..."
 				if [[ -d /opt/${_TLA,,}  ]] ; then
-					mv /opt/${_TLA,,} ${_BackupFolderName:="/opt/${_TLA,,}.$(date +%Y-%m-%d-%H-%M-%S-%s).bakup"}
-					zip -m -r -5 ${_BackupFolderName}.zip  ${_BackupFolderName}
-					rm -r ${_BackupFolderName}
+					mv /opt/"${_TLA,,}" "${_BackupFolderName:=/opt/${_TLA,,}.$(date +%Y-%m-%d-%H-%M-%S-%s).bakup}"
+					zip -m -r -5 "${_BackupFolderName}".zip  "${_BackupFolderName}"
+					rm -r "${_BackupFolderName}"
 				fi
-				mv /opt/${_TLA,,}.tmp /opt/${_TLA,,} ; rm -rf /opt/${_TLA,,}/.git
-				source /opt/${_TLA,,}/core/_rtd_library
+				mv /opt/"${_TLA,,}".tmp /opt/"${_TLA,,}" ; rm -rf /opt/"${_TLA,,}"/.git
+				source "/opt/${_TLA,,}/core/_rtd_library"
 				oem::register_all_tools
-				ln -s -f ${_LOG_DIR} -T ${_OEM_DIR}/log
-				bash ${_OEM_DIR}/core/rtd-oem-linux-config.sh ${@}
+				ln -s -f "${_LOG_DIR}" -T "${_OEM_DIR}"/log
+				bash "${_OEM_DIR}"/core/rtd-oem-linux-config.sh "${*}"
 			else
 				echo "Failed to retrieve instructions correctly! "
-				echo "Suggestion: check write permission in "/opt" or internet connectivity."
-				exit 1
+				echo "Suggestion: check write permission in /opt or internet connectivity."
+				return 1
 			fi
-		exit $?
-	elif [[ "$OSTYPE" == "darwin"* ]]; then
-		echo "Mac OSX is currently not supported..."
-	elif [[ "$OSTYPE" == "cygwin" ]]; then
-		echo "CYGWIN is currently unsupported..."
-	elif [[ "$OSTYPE" == "msys" ]]; then
-		echo "Lightweight shell is currently unsupported... "
-	elif [[ "$OSTYPE" == "freebsd"* ]]; then
-		echo "Free BSD is currently unsupported... "
-	else
-	echo "This system is Unknown to this script"
-	fi
-	exit $?
+		elif [[ "$OSTYPE" == "darwin"* ]]; then
+			echo "Mac OSX is currently not supported..."
+		elif [[ "$OSTYPE" == "cygwin" ]]; then
+			echo "CYGWIN is currently unsupported..."
+		elif [[ "$OSTYPE" == "msys" ]]; then
+			echo "Lightweight shell is currently unsupported... "
+		elif [[ "$OSTYPE" == "freebsd"* ]]; then
+			echo "Free BSD is currently unsupported... "
+		else
+			echo "This system is Unknown to this script"
+		fi
+	;;
+	* ) write_error "No action requested..."
+	esac
 }
 
 
@@ -215,15 +223,15 @@ dependency::theme_payload ()
 #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 dependency::file _rtd_library && for i in ${_potential_dependencies} ; do check_dependencies  "${i}" ; done
-
+[[ -d "/opt/${_TLA}/themes"  ]] || dependency::theme_payload --download
 
 case $1 in
 	--gtk | --gnome )
-		echo "Foced install of GTK themes..."
+		system::log_item "Foced install of GTK themes..."
 		theme::add_global --gtk
 	;;
 	--kde | --plasma )
-		echo "Foced install of KDE themes..."
+		system::log_item "Foced install of KDE themes..."
 		theme::add_global --kde
 	;;
 	--all )
@@ -235,40 +243,42 @@ case $1 in
 		theme::add_global --bash
 	;;
 	--icons | --icon)
-		echo "Installing icons only..."
+		system::log_item "Installing icons only..."
 		theme::add_global --icon
 	;;
 	--fonts | --font )
-		echo "Installing fonts only"
+		system::log_item "Installing fonts only"
 		theme::add_global --font
 	;;
 	--bash | --term | --terminal )
-		echo "Installing bash theme only"
+		system::log_item "Installing bash theme only"
 		theme::add_global --bash
 	;;
 	--wallpaper | --backgrounds | --images )
-		echo "Installing Wallpapers only"
+		system::log_item "Installing Wallpapers only"
 		theme::add_global --wallpaper
 	;;
 	--help )
 		theme::help
 	;;
 	* )
-		echo "No preference stated. Autodetecting themes for current environment..."
-		if  ps -e |grep "plasmashell" ; then
+		system::log_item "No preference stated. Autodetecting themes for current environment..."
+		if  pgrep -f "plasmashell" ; then
+			system::log_item "Found plasmashell; installing kde themes, icons, fonts, bash theme, and wallpapers..."
 			theme::add_global --kde
 			theme::add_global --icon
 			theme::add_global --font
 			theme::add_global --bash
 			theme::add_global --wallpaper
-		elif  ps -e |grep "gnome-shell"; then
+		elif  pgrep -f "gnome-shell"; then
+			system::log_item "Found gnome-shell; installing gnome themes, icons, fonts, bash theme, and wallpapers..."
 			theme::add_global --gtk
 			theme::add_global --icon
 			theme::add_global --font
 			theme::add_global --bash
 			theme::add_global --wallpaper
 		else
-			echo "Neither plasma or gnome was found! Only installing Icons and fonts."
+			system::log_item "Neither plasma or gnome was found! Only installing Icons, wallpapers and fonts."
 			theme::add_global --icon
 			theme::add_global --font
 			theme::add_global --wallpaper
